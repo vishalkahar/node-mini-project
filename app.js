3c19a8d600a7c323a10d3d5fa2ec834180c7087e
vishalkahar <vishalkahar07@gmail.com>
2024-12-19 00:30:02 +0530

    Add CLI file creation script and update dependencies
    Implemented a script that prompts the user for a file name and content, then creates a file with the provided information. Updated package.json to include the readline dependency and added a README.md file describing the project.

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-/* Check another branch for wonderful code */
+import readline from "readline";
+import fs from "fs";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const fileCreation = () => {
+    rl.question("Enter your file name: ", (fileName) => {
+        rl.question("Enter your content for your file: ", (fileContent) => {
+            fs.writeFile(`${fileName}.txt`, fileContent, (err) => {
+                if (err) {
+                    console.error(`Error writing file: ${err.message}`);
+                } else {
+                    console.log(`File "${fileName}.txt" created successfully`);
+                }
+                rl.close();
+            });
+        });
+    });
+};
+
+fileCreation();
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Node JS Project #2: Create a CLI File Creation App Using Node.js
+
+## Description
+
+Implemented a script that prompts the user for a file name and content, then creates a file with the provided information. Updated package.json to include the readline dependency.
+
+## Technologies Used
+
+- Node.js
+- readline
+- fs
+
