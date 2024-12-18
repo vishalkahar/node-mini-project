30e88b66d66f43d8d4386ed031391997c5ba534e
vishalkahar <vishalkahar07@gmail.com>
2024-12-19 00:40:35 +0530

    Add random joke generator functionality
    Implemented a script in `app.js` that fetches a random joke from an API and displays it in the console using the `https` module and `chalk` for colorized output. Updated `package.json` to include `chalk` as a dependency and created a `readme.md` file to describe the project and technologies used.

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-/* Check another branch for wonderful code */
+import https from "https";
+import chalk from "chalk";
+
+
+const getJoke = () => {
+    const url = "https://official-joke-api.appspot.com/random_joke";
+    https.get(url, (response) => {
+        let data = "";
+        response.on("data", (chunk) => {
+            data += chunk;
+        });
+
+        response.on("end", () => {
+            const joke = JSON.parse(data);
+            console.log(`Here is a random ${joke.type} joke:`);
+            console.log(chalk.red(joke.setup));
+            console.log(chalk.green(joke.punchline));
+        });
+
+        response.on("error", (error) => {
+            console.error(`Error fetching joke: ${error.message}`);
+        });
+    });
+};
+
+getJoke();
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Project 3: Create a Random Joke Generator App in Node.js
+
+## Description
+
+Implemented a script that fetches a random joke from an API and displays it in the console. Used the `https` module to make HTTP requests and the `chalk` module to colorize the output.
+
+## Technologies Used
+
+- Node.js
+- https
+- chalk
